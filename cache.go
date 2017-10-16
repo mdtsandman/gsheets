b77c49089e5abbf13b58b633e036676e8c495bf4
mdtsandman <mdtsandman@gmail.com>
2017-10-16 06:29:35 -0700

    Added safe cache. Added mutexes to grid.

---
+++ cache.go
@@ -0,0 +1,62 @@
+package gsheets
+
+import (
+	"fmt"
+	"sync"
+)
+
+import (
+	"google.golang.org/api/sheets/v4"
+)
+
+type Meta struct {
+	ssid, rngStr string
+	ok bool
+	mtx sync.Mutex
+}
+
+func NewMeta(ssid, rngStr string) (meta Meta) {
+	return Meta{
+		ssid: ssid,
+		rngStr: rngStr,
+		ok: false,
+	}
+}
+
+type Cache struct{
+	meta map[string]Meta
+	data map[string]Grid
+	mtx sync.Mutex
+}
+
+func NewCache() (cache Cache) {
+	cache.meta = make(map[string]Meta)
+	cache.data = make(map[string]Grid)
+	return cache
+}
+
+func (sc Cache) SetMeta(resource string, meta Meta) {
+	sc.mtx.Lock()
+	defer sc.mtx.Unlock()
+	sc.meta[resource] = meta
+}
+
+func (sc Cache) Fetch(srv *sheets.Service, resource string) (g Grid, cached bool, err error) {
+	sc.mtx.Lock()
+	defer sc.mtx.Unlock()
+	meta := sc.meta[resource]
+	if meta.ok {
+		fmt.Printf("Using cached data for %s\n", meta.rngStr)
+		return sc.data[resource], true, nil
+	}
+	fmt.Printf("No cached data found for %s\nFetching from spreadsheet via REST API\n", meta.rngStr)
+	req := srv.Spreadsheets.Values.Get(meta.ssid, meta.rngStr)
+	resp, err := req.ValueRenderOption("UNFORMATTED_VALUE").DateTimeRenderOption("SERIAL_NUMBER").Do()
+	if err != nil {
+		return g, false, err
+	}
+	sc.data[resource] = NewGrid(resp.Values)
+	meta.ok = true
+	sc.meta[resource] = meta
+	return sc.data[resource], false, nil
+}
--- grid.go
+++ grid.go
@@ -1,14 +1,18 @@
 package gsheets
 
+import (
+	"sync"
+)
+
 type Grid struct {
 	colHdr, rowHdr map[string]int
 	data [][]interface{}
+	mtx sync.Mutex
 }
 
-func NewGrid(data [][]interface{}) (g Grid, ok bool) {
-	if data == nil || len(data) == 0 || len(data[0]) == 0 {
-		return g, false
-	}
+func NewGrid(data [][]interface{}) (g Grid) {
+	g.mtx.Lock()
+	defer g.mtx.Unlock()
 	g.data = data
 	g.colHdr = make(map[string]int)
 	g.rowHdr = make(map[string]int)
@@ -22,27 +26,10 @@ func NewGrid(data [][]interface{}) (g Grid, ok bool) {
 			g.colHdr[key] = j
 		}
 	}
-	return g, true
+	return g
 }
 
-func (g Grid) ColHdr() (h map[string]int, ok bool) {
-	if g.data == nil {
-		return h, false
-	}
-	return g.colHdr, true
-}
-
-func (g Grid) RowHdr() (h map[string]int, ok bool) {
-	if g.data == nil {
-		return h, false
-	}
-	return g.rowHdr, true
-}
-
-func (g Grid) Value(rowHdrStr, colHdrStr string) (value interface{}, ok bool) {
-	if g.data == nil {
-		return value, false
-	}
+func (g Grid) value(rowHdrStr, colHdrStr string) (value interface{}, found bool) {
 	if i, present := g.rowHdr[rowHdrStr]; present {
 		if j, present := g.colHdr[colHdrStr]; present {
 			if j < len(g.data[i]) {
@@ -53,8 +40,16 @@ func (g Grid) Value(rowHdrStr, colHdrStr string) (value interface{}, ok bool) {
 	return value, false
 }
 
-func (g Grid) String(rowHdrStr, colHdrStr string) (value string, ok bool) {
-	if tmp, ok := g.Value(rowHdrStr, colHdrStr); ok {
+func (g Grid) Interface(rowHdrStr, colHdrStr string) (value interface{}, found bool) {
+	g.mtx.Lock()
+	defer g.mtx.Unlock()
+	return g.value(rowHdrStr, colHdrStr)
+}
+
+func (g Grid) String(rowHdrStr, colHdrStr string) (value string, found bool) {
+	g.mtx.Lock()
+	defer g.mtx.Unlock()
+	if tmp, ok := g.value(rowHdrStr, colHdrStr); ok {
 		if value, ok := tmp.(string); ok {
 			return value, true
 		}
@@ -62,11 +57,25 @@ func (g Grid) String(rowHdrStr, colHdrStr string) (value string, ok bool) {
 	return "", false
 }
 
-func (g Grid) Float64(rowHdrStr, colHdrStr string) (value float64, ok bool) {
-	if tmp, ok := g.Value(rowHdrStr, colHdrStr); ok {
-		if value, ok := tmp.(float64); ok {
+func (g Grid) Float64(rowHdrStr, colHdrStr string) (value float64, found bool) {
+	g.mtx.Lock()
+	defer g.mtx.Unlock()
+	if tmp, found := g.value(rowHdrStr, colHdrStr); found {
+		if value, found := tmp.(float64); found {
 			return value, true
 		}
 	}
-	return float64(0), true
+	return float64(0), false
+}
+
+func (g Grid) RowTags() ([]string) {
+	g.mtx.Lock()
+	defer g.mtx.Unlock()
+	var tags []string
+	for _, row := range g.data {
+		if str, ok := row[0].(string); ok {
+			tags = append(tags,str)
+		}
+	}
+	return tags
 }
