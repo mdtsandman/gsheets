80cfe5a86c23a7cb1c33afb6dc436b0502f0fec6
mdtsandman <mdtsandman@gmail.com>
2017-10-23 17:16:31 -0700

    Much improved cache

--- cache.go
+++ cache.go
@@ -12,7 +12,6 @@ import (
 
 type Meta struct {
 	ssid, rngStr string
-	ok bool
 	mtx sync.Mutex
 }
 
@@ -20,58 +19,65 @@ func NewMeta(ssid, rngStr string) (meta Meta) {
 	return Meta{
 		ssid: ssid,
 		rngStr: rngStr,
-		ok: false,
 	}
 }
 
 type Cache struct{
+	srv *sheets.Service
 	meta map[string]Meta
 	data map[string][][]interface{}
 	mtx sync.Mutex
 }
 
-func NewCache() (cache Cache) {
-	cache.meta = make(map[string]Meta)
-	cache.data = make(map[string][][]interface{})
-	return cache
+func NewCache(srv *sheets.Service) (c Cache) {
+	c.srv = srv
+	c.meta = make(map[string]Meta)
+	c.data = make(map[string][][]interface{})
+	return c
 }
 
-func (c Cache) SetChangedFlag(resource string) (cache Cache) {
+func (c Cache) Register(resource, ssid, rngStr string) (cache Cache, err error) {
 	c.mtx.Lock()
-	defer c.mtx.Unlock()
-	if meta, exists := c.meta[resource]; exists {
-		meta.ok = false
-		c.meta[resource] = meta
-	}
-	return cache
+	c.meta[resource] = NewMeta(ssid, rngStr)
+	c.mtx.Unlock()
+	_, err = c.Fetch(resource)
+	return c, err
 }
 
-func (c Cache) SetMeta(resource string, meta Meta) (cache Cache) {
+func (c Cache) Data(resource string) (data [][]interface{}, err error) {
 	c.mtx.Lock()
 	defer c.mtx.Unlock()
-	c.meta[resource] = meta
-	return cache
+	_, registered := c.meta[resource]
+	if (!registered) {
+		return data, errors.New(fmt.Sprintf("Attempt to read unregistered resource [%s].", resource))
+	}
+	return c.data[resource], nil
 }
 
-func (c Cache) Fetch(srv *sheets.Service, resource string, force bool) (data [][]interface{}, err error) {
+func (c Cache) Fetch(resource string) (data [][]interface{}, err error) {
 	c.mtx.Lock()
 	defer c.mtx.Unlock()
-	meta, exists := c.meta[resource]
-	if (!exists) {
-		return data, errors.New(fmt.Sprintf("Attempt to fetch resource [%s], which is not registered in the cache.",resource))
+	meta, registered := c.meta[resource]
+	if (!registered) {
+		return data, errors.New(fmt.Sprintf("Attempt to fetch unregistered resource [%s].", resource))
 	}
-	if !force && meta.ok {
-		fmt.Printf("Using cached data for resource [%s]\n", meta.rngStr)
-		return c.data[resource], nil
-	}
-	fmt.Printf("Fetching resource [%s] from spreadsheet via REST API\n", meta.rngStr)
-	req := srv.Spreadsheets.Values.Get(meta.ssid, meta.rngStr)
+	fmt.Printf("Fetching %s from spreadsheet %s via REST API\n", meta.rngStr, meta.ssid)
+	req := c.srv.Spreadsheets.Values.Get(meta.ssid, meta.rngStr)
 	resp, err := req.ValueRenderOption("UNFORMATTED_VALUE").DateTimeRenderOption("SERIAL_NUMBER").Do()
 	if err != nil {
 		return data, err
 	}
 	c.data[resource] = resp.Values
-	meta.ok = true
-	c.meta[resource] = meta
 	return c.data[resource], nil
+}
+
+func (c Cache) Update(resource string, data [][]interface{}) (cache Cache, err error) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	_, registered := c.meta[resource]
+	if (!registered) {
+		return c, errors.New(fmt.Sprintf("Attempt to update unregistered resource [%s].", resource))
+	}
+	c.data[resource] = data
+	return c, nil
 }
